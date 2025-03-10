3a9b4755ee0427602be22ab16616541365f209cd
Jonas <Pacheco.Blanco.Jonas@gmail.com>
2025-03-10 23:46:11 +0100

    change mysql for mariadb

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,6 +1,6 @@
-import mysql from "mysql2/promise";
+import maria from "mariadb";
 
-const pool = mysql.createPool({
+const pool = maria.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
